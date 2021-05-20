7097c7ab05219cd0a080118e004dfa4bc0254621
edisaurus <edswork.ece@gmail.com>
2021-05-20 19:16:59 +0800

    add mount_uploaders and validation for max file size upload

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
+  mount_uploaders :images, ImageUploader
   belongs_to :user
   validates :name, :breed, :specie, :birthdate, :age, presence: true
+  validates :images, file_size: { less_than: 1.megabytes }
 end
