5a2b0104ca00a8034d867c9cc80ffcc4e39c5fb4
edisaurus <edswork.ece@gmail.com>
2021-05-24 18:15:02 +0800

    add skip callback

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,7 @@
 class Pet < ApplicationRecord
   attr_accessor :images_cache
   mount_uploaders :images, ImageUploader
-  # skip_callback :commit, :after, :remove_previously_stored_images
+  skip_callback :commit, :after, :remove_previously_stored_images
   belongs_to :user
   validates :name, :breed, :specie, :birthdate, :age, presence: true
   validates :images, file_size: { less_than: 1.megabytes }
