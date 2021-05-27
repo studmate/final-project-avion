c6a6b38639fd1b8ed5d96a728f4f5f19528a7fea
edisaurus <edswork.ece@gmail.com>
2021-05-28 06:04:44 +0800

    refactor config for aws region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_provider = 'fog/aws'                        # required
+    config.fog_public = true
+    config.storage = :fog
     config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
       aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-      region: 'ap-southeast-1'                             # Singapore
+      region:                ENV["AWS_REGION"]                            
     }
     config.fog_directory  = ENV["AWS_BUCKET"]              # required
-    config.storage = :fog
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
