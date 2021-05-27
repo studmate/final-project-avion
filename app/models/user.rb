d2c7af82d6bfd9d6e56837285660ef15fb5d3494
edisaurus <edswork.ece@gmail.com>
2021-05-27 23:30:49 +0800

    update devise registrations and add username parameter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :signin
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    signin = conditions.delete(:signin)
+    where(conditions)
+    .where(["lower(username) = :value OR
+             lower(email) = :value",
+             { value: signin.to_s.downcase.gsub(/\s+/, '')}]).first
+  end
+
   has_many :pets
-  validates :email, :password, :password_confirmation, presence: true
-  validates :email, uniqueness: true
+  validates :username, :email, :password, :password_confirmation, presence: true
+  validates :username, :email, uniqueness: true
 end
