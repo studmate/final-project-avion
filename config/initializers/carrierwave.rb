e8cbd9ea5d889414e30870b72fee06b277c5e681
edisaurus <edswork.ece@gmail.com>
2021-05-27 23:58:54 +0800

    set carrierwave aws s3 storage config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  if Rails.env.staging? || Rails.env.production?
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+      region: 'ap-southeast-1'                             # Singapore
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+    config.storage
+  else
+    config.storage = :file
+    config.enable_processing = Rails.env.development?
+  end
 end
