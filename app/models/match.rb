f0aae62af1fed841b2f05b869b95617909adac51
edisaurus <edswork.ece@gmail.com>
2021-06-02 17:49:08 +0800

    add asscociations and scope to match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,8 @@
 class Match < ApplicationRecord
-  belongs_to :requestor, class_name: :Pet
-  belongs_to :receiver, class_name: :Pet
+  belongs_to :requestor, class_name: :Pet, foreign_key: :requestor_id
+  belongs_to :receiver, class_name: :Pet, foreign_key: :receiver_id
+  
+  scope :match_true, -> { where(‘status =?’, true) }
+  scope :match_false, -> { where(‘status =?’, false) }
+
 end
