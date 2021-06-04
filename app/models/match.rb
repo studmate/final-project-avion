c339d6de0d617a37e1803fa78d62a27f69ac4245
edisaurus <edswork.ece@gmail.com>
2021-06-04 17:43:12 +0800

    refactor scope for match status

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,6 @@ class Match < ApplicationRecord
   belongs_to :requestor, class_name: :Pet, foreign_key: :requestor_id
   belongs_to :receiver, class_name: :Pet, foreign_key: :receiver_id
   
-  scope :match_true, -> { where(‘status =?’, true) }
-  scope :match_false, -> { where(‘status =?’, false) }
-
+  scope :match_true, -> { where( 'status =?', true) }
+  scope :match_false, -> { where( 'status =?', false) }
 end
