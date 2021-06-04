7a74757919791a629ebeb74f6364190302938248
edisaurus <edswork.ece@gmail.com>
2021-06-04 17:49:44 +0800

    refactor associations for matches

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,12 +4,13 @@ class Pet < ApplicationRecord
   skip_callback :commit, :after, :remove_previously_stored_images
 
   belongs_to :user
-  has_many :matches_as_requestor, foreign_key: :requestor_id, class_name: :Match, dependent: :destroy
-  has_many :matches_as_receiver, foreign_key: :receiver_id, class_name: :Match, dependent: :destroy
+
+  has_many :matches_sent, foreign_key: :requestor_id, class_name: 'Match', inverse_of: 'requestor', dependent: :destroy
+  has_many :matches_request, foreign_key: :receiver_id, class_name: 'Match', inverse_of: 'receiver', dependent: :destroy
   
-  has_many :matches, -> { merge(Match.match_true) }, through: :matches_as_requestor, source: :requestor
-  has_many :pending_matches, -> { merge(Match.match_false) }, through: :matches_as_requestor, source: :requestor
-  has_many :received_matches, -> { merge(Match.match_false) }, through: :matches_as_receiver, source: :receiver
+  has_many :success_matches, -> { merge(Match.match_true) }, through: :matches_sent, source: :receiver
+  has_many :pending_matches, -> { merge(Match.match_false) }, through: :matches_sent, source: :receiver
+  has_many :received_matches, -> { merge(Match.match_false) }, through: :matches_request, source: :requestor
 
   validates :name, :breed, :specie, :birthdate, :age, presence: true
   validates :images, file_size: { less_than: 1.megabytes }
