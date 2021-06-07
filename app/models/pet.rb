e648246f5a1673f70ba458c828176f2db9370356
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-06-07 11:12:26 +0800

    remove birthdate field

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,6 +3,6 @@ class Pet < ApplicationRecord
   mount_uploaders :images, ImageUploader
   skip_callback :commit, :after, :remove_previously_stored_images
   belongs_to :user
-  validates :name, :breed, :specie, :birthdate, :age, presence: true
+  validates :name, :breed, :specie, :age, presence: true
   validates :images, file_size: { less_than: 1.megabytes }
 end
