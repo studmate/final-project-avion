f63c62347216909a80db98a3e83484e942e16611
Marnoel de Guzman <marnoeldeguzman@gmail.com>
2021-06-07 11:23:05 +0800

    change root path to pets index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get 'page/index'
   devise_for :users
-  root 'home#index'
+  root 'pets#index'
   get 'home/index'
   get 'home/about'
-  
   resources :pets
 end
