e1590e3e8da291f9ca26f05da7dd755eb8897d0a
edisaurus <edswork.ece@gmail.com>
2021-06-11 02:17:08 +0800

    merge conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 5.times do
   users = User.create!(
     username: Faker::Internet.username,
@@ -19,31 +18,8 @@
       vaccinated: 'Yes',
       images: [],
     )  
-end  
 end
 
 
 
 
-=======
-# 5.times do
-#   users = User.create!(
-#     username: Faker::Internet.username,
-#     email: Faker::Internet.email,
-#     password: 'password',
-#     password_confirmation: 'password' 
-#   )
-#   2.times do
-#     users.pets.create(
-#       name: Faker::Name.first_name,
-#       specie: 'dog',
-#       breed: 'doge',
-#       age: 1,
-#       birthdate: Faker::Date.between(from: 360.days.ago, to: Date.today),
-#       gender: 'Male',
-#       vaccinated: 'Yes',
-#       images: [],
-#     )  
-#   end  
-# end
->>>>>>> origin/backend-test-matches
