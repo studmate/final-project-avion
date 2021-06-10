6764f1219fda81f1897f7cf949db81f8e6dc8f12
edisaurus <edswork.ece@gmail.com>
2021-06-11 04:59:44 +0800

    install yarn dependencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,25 @@
-5.times do
-  users = User.create!(
-    username: Faker::Internet.username,
-    email: Faker::Internet.email,
-    password: 'password',
-    password_confirmation: 'password', 
-    city: 'Manila',
-    country: 'Philippines'
-  )
-  2.times do
-    users.pets.create(
-      name: Faker::Name.first_name,
-      specie: 'dog',
-      breed: 'doge',
-      age: 1,
-      birthdate: Faker::Date.between(from: 360.days.ago, to: Date.today),
-      gender: 'Male',
-      vaccinated: 'Yes',
-      images: [],
-    )  
-end
+# 5.times do
+#   users = User.create!(
+#     username: Faker::Internet.username,
+#     email: Faker::Internet.email,
+#     password: 'password',
+#     password_confirmation: 'password', 
+#     city: 'Manila',
+#     country: 'Philippines'
+#   )
+#   2.times do
+#     users.pets.create(
+#       name: Faker::Name.first_name,
+#       specie: 'dog',
+#       breed: 'doge',
+#       age: 1,
+#       birthdate: Faker::Date.between(from: 360.days.ago, to: Date.today),
+#       gender: 'Male',
+#       vaccinated: 'Yes',
+#       images: [],
+#     )  
+# end
+# end
 
 
 
